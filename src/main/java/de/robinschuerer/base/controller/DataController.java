d7f1bd4006057441100a754f28607aaa6669bb20
Robin Schürer <robin.schuerer@gmail.com>
2017-06-08 07:12:30 +0200

    cross origin

--- src/main/java/de/robinschuerer/base/controller/DataController.java
+++ src/main/java/de/robinschuerer/base/controller/DataController.java
@@ -2,6 +2,7 @@ package de.robinschuerer.base.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import de.robinschuerer.norcom.searchservice.SearchService;
 
 @RestController
 @RequestMapping("/data")
+@CrossOrigin(origins = "http://localhost:4200")
 public class DataController {
 
     @Autowired
